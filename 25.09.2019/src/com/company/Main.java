0e83eb6db0c889f288d9b10ef36f7b6af23fc867
RuslanStepanov2000 <47542671+RuslanStepanov2000@users.noreply.github.com>
2019-09-25 11:34:31 +0300

    update

---
+++ 25.09.2019/src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue1();
+        Stack1();
+
+    }
+
+    static void Queue1() {
+        int N = 5457123;
+        int N1=N;
+        LinkedList queue = new LinkedList<Integer>();
+        LinkedList queueRev= new LinkedList<Integer>();
+
+        for (int i = 0; i < 7; i++) {// Количество цифр в числt
+            queue.add(N % 10);
+            N = ((N - N % 10) / 10);
+            queueRev.add(N1/(int)Math.pow(10,6-i));
+            N1=(N1 % (int)Math.pow(10, 6-i));
+
+
+        }
+        System.out.println("Queue "+queue);
+        System.out.println("QueueRev "+queueRev);
+
+    }
+    static void Stack1(){
+        int N = 5457123;
+        int N1=N;
+        Stack stack = new Stack();
+        Stack stackRev = new Stack();
+
+        for (int i = 0; i < 7; i++) {
+            stackRev.add(N % 10);
+            N = ((N - N % 10) / 10);
+            stack.add(N1 / (int) Math.pow(10, 6 - i));
+            N1 = (N1 % (int) Math.pow(10, 6 - i));
+        }
+        System.out.println("ReveseStack"+stackRev);
+        System.out.println("Stack"+stack);
+    }
+
+
+
+
+}
+
+
+
