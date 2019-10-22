106aeebc6f337c2fd2c138a9da23e20c08acc024
RuslanStepanov2000 <47542671+RuslanStepanov2000@users.noreply.github.com>
2019-10-22 14:54:49 +0300

    update RegEx

--- хх.10.2019(Задание 4)/src/com/company/exp.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-public class exp {
-    public static void main(String[] args) {
-        Methods regex = new Methods();
-        String str="qwertyuiop qwertyuiopp wqwertyuioppp w wq wqqq qqqqq";
-        regex.replaceString(str);
-    }
-}
