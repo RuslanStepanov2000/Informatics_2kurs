45c0e515719fbd6066a7e3237bec05b9d00c60b4
RuslanStepanov2000 <47542671+RuslanStepanov2000@users.noreply.github.com>
2019-11-06 10:25:52 +0300

    Закончил задание 3

--- zadanie3 09.10.2019/src/com/company/Main.java
+++ zadanie3 09.10.2019/src/com/company/Main.java
@@ -4,16 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         //Номер 1
-	newclass.readAndPrintXML();
-	    //Номер 2
-    Sport sp=new Sport();
-    sp.readDoc("nomer2.xml");
+        newclass.readAndPrintXML();
+        //Номер 2
+        Sport sp = new Sport();
+        sp.readDoc("nomer2.xml");
         System.out.println("Successful read");
-    sp.print3a();
+        sp.print3a();
         System.out.println("Successful 3a");
-    sp.print3b();
+        sp.print3b();
         System.out.println("Successful 3b");
-    sp.print3c();
+        sp.print3c();
         System.out.println("Successful 3c");
+        sp.inputAthlete();
+        System.out.println("Successful 4");
+        sp.newXmlFile();
+        System.out.println("Successful 5");
     }
 }
--- zadanie3 09.10.2019/src/com/company/Sport.java
+++ zadanie3 09.10.2019/src/com/company/Sport.java
@@ -1,16 +1,22 @@
 package com.company;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Sport {
     private List<Person> personList = null;
@@ -119,4 +125,122 @@ public class Sport {
         String[] s = str.split("-");
         return Integer.parseInt(s[0]);
     }
+
+    public void inputAthlete() {
+        String name;
+        String birtday;
+        String gender;
+        List<Event> eventList = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя спортсмена:");
+        name = scanner.next();
+        System.out.print("Введите дату рождения cпортсмена (гггг-мм-дд): ");
+        birtday = scanner.next();
+        while (true){
+            System.out.print("Введите пол (м/ж): ");
+            gender = scanner.next();
+            if (gender.equals("м")||gender.equals("ж")){
+                break;
+            }
+        }
+        while (true) {
+            System.out.println("Хотите добавить соревнования портсмену? (да/нет)");
+            String fl = scanner.next();
+            if (fl.equals("да")) {
+                String place;
+                int year;
+                int resoult;
+                String award;
+
+                System.out.print("Введите место проведения соревнования: ");
+                place=scanner.next().toLowerCase();
+                while (true){
+                    System.out.print("Введите год проведения соревнования: ");
+                    String yearString=scanner.next();
+                    if (isNumber(yearString)){
+                        year=Integer.parseInt(yearString);
+                        break;
+                    }
+                }
+                while (true){
+                    System.out.print("Введите результат спортсмена на соревнованиях: ");
+                    String resoultString=scanner.next();
+                    if (isNumber(resoultString)){
+                        resoult=Integer.parseInt(resoultString);
+                        break;
+                    }
+                }
+                System.out.print("Введите полученную медаль на соревнованиях (gold/silver/bronze/no medals): ");
+                award=scanner.next();
+
+                Event event=new Event();
+                event.setPlace(place);
+                event.setYear(year);
+                event.setAward(award);
+                event.setResult(resoult);
+                eventList.add(event);
+            } else if (fl.equals("нет")) {
+                break;
+            } else {
+                continue;
+            }
+        }
+        Person Person=new Person();
+        Person.setName(name);
+        Person.setBirthday(birtday);
+        Person.setSex(gender);
+        Person.setEventList(eventList);
+        System.out.println(Person.toString());
+    }
+
+    private boolean isNumber(String s){
+        try{
+            Integer.parseInt(s);
+            return true;
+        }catch (NumberFormatException e){
+            return false;
+        }
+    }
+    public List<Person> getPersonList() {
+        return personList;
+    }
+
+    public void newXmlFile(){
+        try {
+            Document document=DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+            Element team=document.createElement("team");
+            document.appendChild(team);
+            for (Person a:personList){
+                Element Person=document.createElement("sportsmen");
+                Attr name=document.createAttribute("name");
+                name.setTextContent(a.getName());
+                Attr numberOfEvent=document.createAttribute("numberOfEvent");
+                numberOfEvent.setTextContent(String.valueOf(a.getEventList().size()));
+                List<Event> eventList=a.getEventList();
+                int sumResoultInt=0;
+                for (Event event:eventList){
+                    sumResoultInt+=event.getResult();
+                }
+                Attr sumResoult=document.createAttribute("sumResoult");
+                sumResoult.setTextContent(String.valueOf(sumResoultInt));
+                Person.setAttributeNode(name);
+                Person.setAttributeNode(numberOfEvent);
+                Person.setAttributeNode(sumResoult);
+                team.appendChild(Person);
+            }
+
+            Transformer transformer= TransformerFactory.newInstance().newTransformer();
+            DOMSource source=new DOMSource(document);
+            StreamResult result=new StreamResult(new File("newFile.xml"));
+            transformer.transform(source, result);
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
