f36c18c3d002ff35c4512bb7438cc997771170de
RuslanStepanov2000 <47542671+RuslanStepanov2000@users.noreply.github.com>
2019-11-06 11:02:54 +0300

    update

--- 16.10.2019(Задание 5)/src/com/company/Main.java
+++ 16.10.2019(Задание 5)/src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-
-import jdk.nashorn.internal.parser.JSONParser;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -34,6 +32,7 @@ public class Main {
             System.out.println(String.format("Имя: %s, Дата рождения: %s, Пол: %s, Соревнования: %s", person.getName(), person.getBirthday(), person.getSex(), person.getEventList()));
         }
         System.out.println("Cycle end");
+
     }
 
     static class XMLHandler extends DefaultHandler {
@@ -61,46 +60,3 @@ public class Main {
         }
     }
 }
-
-
-    /*
-
-        for (int i = 0; i < personList.getLength(); i++) {
-            if (Person.getNodeName().equals("sportsman")) {
-                //Заполнение информации о человеке
-                Person thisPerson = new Person();
-                thisPerson.setName();
-                thisPerson.setBirthday();
-                thisPerson.setSex();
-
-                //Заполнение информации о соревнованиях
-                List<Event> events = new ArrayList<>();
-
-
-                for (int j = 0; j < events.size(); j++) {
-                    Node event = events.indexOf(j);
-                    if (event.getNodeName().equals("event")) {
-                        Event thisEvent = new Event();
-                        thisEvent.setPlace();
-                        thisEvent.setYear();
-                        thisEvent.setResult();
-                        thisEvent.setAward();
-                        events.add(thisEvent);
-                    }
-                }
-                thisPerson.setEventList(events);
-                this.personList.add(thisPerson);
-
-
-    }
-    private static class XMLHandler extends DefaultHandler {
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            if (qName.equals("employee")) {
-                String name = attributes.getValue("name");
-                String job = attributes.getValue("job");
-                employees.add(new Employee(name, job));
-            }
-        }
-
-        */
