6815e9b999881053039492e505021a96d08bc2e5
RuslanStepanov2000 <47542671+RuslanStepanov2000@users.noreply.github.com>
2019-12-16 00:19:11 +0300

    ура, обманул финам, ява прикидывается браузером

--- Kursa4/src/test3.java
+++ Kursa4/src/test3.java
@@ -1,31 +1,25 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
+import java.io.*;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
 
-public class testclass {
-    public static void main(String[] args) throws IOException, IOException {
+public class test3 {
+
+    public static void main(String[] args) throws IOException {
 
         String urlString = "http://export.finam.ru/POLY_170620_170623.txt?market=1&em=175924&code=POLY&apply=0&df=20&mf=5&yf=2017&from=20.06.2017&dt=23&" +
                 "mt=5&yt=2017&to=23.06.2017&p=8&f=POLY_170620_170623&e=.txt&cn=POLY&dtf=1&tmf=1&" +
                 "MSOR=1&mstime=on&mstimever=1&sep=1&sep2=1&datf=1&at=1";
-        URL url = new URL(urlString);
 
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
-        conn.setUseCaches(false);
-        conn.setConnectTimeout(60000);
-        conn.setRequestMethod("GET");
+        URLConnection conn = new URL(urlString).openConnection();
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+        conn.connect();
 // подготовили коннект
 
 // Т.к. запрос у нас GET, то сразу принимаем входящие данные.
 // Вот тут как раз (при открытии InputStream ) и происходит отправка GET запроса на сервер.
-        InputStream inStream = conn.getInputStream();
 // Т.к. файл у нас бинарный, открываем ReadableByteChannel и создаем файл
         ReadableByteChannel rbc = Channels.newChannel(conn.getInputStream());
         FileOutputStream fos = new FileOutputStream("input.csv");
@@ -35,7 +29,7 @@ public class testclass {
         long filePosition = 0;
         long transferedBytes = fos.getChannel().transferFrom(rbc, filePosition, Long.MAX_VALUE);
 
-        while(transferedBytes == Long.MAX_VALUE){
+        while (transferedBytes == Long.MAX_VALUE) {
             filePosition += transferedBytes;
             transferedBytes = fos.getChannel().transferFrom(rbc, filePosition, Long.MAX_VALUE);
         }
